14db5f15adbf3f65e42408d6b6d62a04dbe01b0c
Gan356RSM <151418866+Gan356RSM@users.noreply.github.com>
2024-01-28 11:48:11 +0700

    Update pro.py

--- weird_stuff/pro.py
+++ weird_stuff/pro.py
@@ -1,22 +1,36 @@
 from PyQt5.QtWidgets import *
 
 dic = {}
+select_word = ""
 
 app = QApplication([])
 win = QWidget()
 
 def Add():
     word, ok = QInputDialog.getText(win, "Add Word", "Enter word:")
+    dic[word] = ""
     if ok:
         lw.addItem(word)
 
+def Delete():
+    text_e.clear()
+
 def Selective_Items():
+    global dic
+    global select_word
+    
+    watever = text_e.toPlainText()
+
+    if select_word != "":
+        dic[select_word] = watever
+
     items = lw.selectedItems()
     if len(items) == 0:
         return
 
     item = items[0].text()
-    dic[item] = text_e.setPlainText("cat")
+    text_e.setPlainText(dic[item])
+    select_word = item
 
 
 lw = QListWidget()
@@ -28,6 +42,7 @@ butt2 = QPushButton("Delete")
 butt3 = QPushButton("Save")
 
 butt1.clicked.connect(Add)
+butt2.clicked.connect(Delete)
 
 laysV1 = QVBoxLayout()
 laysV2 = QVBoxLayout()
